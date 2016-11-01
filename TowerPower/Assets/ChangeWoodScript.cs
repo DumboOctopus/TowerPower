52622755ca93119be8c6f93b8cdeb806539176fe
banding123 <andrewn558@gmail.com>
2016-11-01 13:53:54 -0700

    Update ChangeWoodScript.cs

--- TowerPower/Assets/ChangeWoodScript.cs
+++ TowerPower/Assets/ChangeWoodScript.cs
@@ -4,8 +4,11 @@ using UnityEngine.UI;
 
 public class ChangeWoodScript : MonoBehaviour {
 
+	// the thickness of the member
 	public float woodThickness;
+	// the button that changes the wood
 	private Button btn;
+	// the text on the button
 	private Text txt;
 
 	// Use this for initialization
@@ -20,7 +23,7 @@ public class ChangeWoodScript : MonoBehaviour {
 	void Update () {
 		
 	}
-
+	// sets text to the type of wood
 	public void TaskOnListener(){
 		if (woodThickness == 0.125) {
 			woodThickness = 1 / 16f;
