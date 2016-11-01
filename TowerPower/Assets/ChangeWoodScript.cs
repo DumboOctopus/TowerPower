f113cb00bf0026c684c9929144fe2bfe94ae975e
Neil Prajapati <neilp0101@gmail.com>
2016-10-31 19:55:43 -0700

    Created Wood size interfaces but not yet implemenetd

---
+++ TowerPower/Assets/ChangeWoodScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChangeWoodScript : MonoBehaviour {
+
+	public double woodThickness;
+	private Button btn;
+	private Text txt;
+
+	// Use this for initialization
+	void Start () {
+		btn = GetComponent<Button> ();
+		txt = GetComponentInChildren<Text> ();
+		btn.onClick.AddListener (TaskOnListener);
+		woodThickness = 0.125;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TaskOnListener(){
+		if (woodThickness == 0.125) {
+			woodThickness = 1 / 16f;
+			txt.text = "Wood (1/16)";
+		} else {
+			woodThickness = 0.125f;
+			txt.text = "Wood (1/8)";
+		}
+		
+	}
+}
