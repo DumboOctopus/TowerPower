a69a4d707384f46a61976d21406629010bb93bfa
banding123 <andrewn558@gmail.com>
2016-11-01 13:56:29 -0700

    Update DragByMouseScript.cs

--- TowerPower/Assets/DragByMouseScript.cs
+++ TowerPower/Assets/DragByMouseScript.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class DragByMouseScript : MonoBehaviour {
 
+	// world point where camera is
 	private Vector3 screenPoint;
+	// difference between camera's poistion and gameObject's position
 	private Vector3 offset;
 
+	// called when the mouse is clicked
 	void OnMouseDown()
 	{
 		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
@@ -13,7 +16,7 @@ public class DragByMouseScript : MonoBehaviour {
 		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
 
 	}
-
+	// when the mouse is being dragged
 	void OnMouseDrag()
 	{
 		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
