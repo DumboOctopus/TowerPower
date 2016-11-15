93d60d50d0d3a8f32008e45ad103766c5d043faf
Neil Prajapati <neilp0101@gmail.com>
2016-11-14 18:53:16 -0800

    Added Added button SCript type of thing

---
+++ TowerPower/Assets/AddDeleteButtonScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AddDeleteButtonScript : MonoBehaviour {
+
+	public bool newState;
+	// the button that switches between delete mode and adding mode
+	public Button btn;
+	// an array that holds all the segments of the tower
+	public SegmentPlacingScript[] allSegments;
+
+	// Use this for initialization
+	void Start () {
+		btn = GetComponent<Button> ();
+		btn.onClick.AddListener (TaskOnListener);
+
+	}
+	//switches to deleting mode
+	void TaskOnListener(){
+
+		allSegments = FindObjectsOfType<SegmentPlacingScript> ();
+		if (allSegments.Length == 0)
+			return;
+
+		for (int i = 0; i < allSegments.Length; i++) {
+			allSegments [i].isAddingMode = newState;
+		}
+
+
+	}
+}
